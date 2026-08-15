837029cbccf8b15bd814f7ec94de81ae749e7862
Rizky Wahyudi <rzkywhyd97@gmail.com>
2026-08-15 17:56:33 +0700

    fix: use database connection URL

--- backend-mdf/config/db.js
+++ backend-mdf/config/db.js
@@ -1,11 +1,25 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: Number(process.env.DB_PORT)
+// });
+
+// module.exports = pool.promise();
+
+const mysql = require("mysql2");
+
+const dbUrl = new URL(process.env.DATABASE_URL);
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT)
+  host: dbUrl.hostname,
+  port: Number(dbUrl.port) || 3306,
+  user: decodeURIComponent(dbUrl.username),
+  password: decodeURIComponent(dbUrl.password),
+  database: dbUrl.pathname.slice(1),
 });
 
 module.exports = pool.promise();
